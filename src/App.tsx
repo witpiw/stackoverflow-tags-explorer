779329cc22eb81a7bf80e969ee4a2302df966a0c
witpiw <piweckiwit@gmail.com>
2024-03-28 22:59:11 +0100

    refactor: remove previous pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import { Table } from "./components/Table"
-import { useParametersStore } from "./hooks/useParametersStore"
 
 function App() {
-  const { setPage, page } = useParametersStore()
 
   return (
-    <>
-      <button onClick={() => setPage(page - 1)}>Prev page</button>
-      <div>{page}</div>
-      <button onClick={() => setPage(page + 1)}>Next page</button>
-      <Table />
-    </>
-
+    <Table />
   )
 }
 
